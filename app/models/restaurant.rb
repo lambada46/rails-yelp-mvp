ad698bbd7389cb3cba6b08a6b2018af189090566
lucas furtado <lambada46@gmail.com>
2017-11-08 11:53:12 -0200

    restaurant validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,7 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  # A restaurant must have at least a name and an address.
+  validates :name, :address, presence: true
+  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"],
+  message: "%{value} is not a valid categoy" }
 end
