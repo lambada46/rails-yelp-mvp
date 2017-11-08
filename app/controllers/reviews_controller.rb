21ccb1f50617dbcafb53a1f9bf11eb3f678b787c
lucas furtado <lambada46@gmail.com>
2017-11-08 15:55:26 -0200

    review - function to create a new one, with its view

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,24 @@
+class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: [:new, :create]
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = @restaurant.reviews.new(review_params)
+    @review.save
+
+    redirect_to @restaurant
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'restaurants#index'
-  resources :restaurants
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
 end
