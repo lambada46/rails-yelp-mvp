d3551d3bb5f9cae408f2ec5fc6a2b5992ede9cab
lucas furtado <lambada46@gmail.com>
2017-11-08 17:36:58 -0200

    changed the formulary to create a new restaurant' git push origin master

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,9 @@
 class Restaurant < ApplicationRecord
+
+  CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
   has_many :reviews, dependent: :destroy
   # A restaurant must have at least a name and an address.
   validates :name, :address, :phone_number,  presence: true
-  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"],
+  validates :category, inclusion: { in: Restaurant::CATEGORIES,
   message: "%{value} is not a valid categoy" }
 end
