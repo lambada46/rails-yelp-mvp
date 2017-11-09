cc30f0797438703b47907a2b54a5f594a790021a
lucas furtado <lambada46@gmail.com>
2017-11-09 13:22:41 -0200

    atualization

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,7 @@
 class Restaurant < ApplicationRecord
 
   CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+
   has_many :reviews, dependent: :destroy
   # A restaurant must have at least a name and an address.
   validates :name, :address, :phone_number,  presence: true
